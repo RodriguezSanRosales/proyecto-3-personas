d3d3dcbc0753f020c944bd5d4865c2038295817a
RodriguezSanRosales <san.rodzrosales@gmail.com>
2019-02-11 20:55:40 -0600

    se agregaron js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-ECHO est  activado.
+const intro = require('./intro');
+const nudo = require('./nudo');
+const final = require('./final');
+
+
+const nombre = "Julio";
+const comida = "pasto";
+const ojoVaca = "X-X";
+const texto = "aaaa!";
+// const edad = Math.floor(Math.random() * 100);
+const edad = 19;
+
+/*otra historia */
+const personaje = 'Kenia';
+const lugar = 'parque';
+const otroP = 'conejo';
+
+//string historia
+// const historia = "Habia una vez una vaca llamada " + nombre +" y todo el tiempo comia " + comida + " con otras vacas hasta que se intoxico y murio."  ;
+// console.log(historia);
+console.log(intro.historia(personaje, lugar, edad) + nudo.historia(otroP, lugar) + final.historia(personaje, lugar));
+
+
+
+console.log(cowsay.say({
+    text: texto,
+    e: ojoVaca,
+    T: "v "
+}));
