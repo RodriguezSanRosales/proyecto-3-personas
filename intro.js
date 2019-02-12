6ff8a56d11d54c7cc5bffc13cd0f81fbc1d94533
keep in touch <kit@MacBook-Pro-de-keep-2.local>
2019-02-11 21:13:31 -0600

    inicio de historia

---
+++ intro.js
@@ -0,0 +1,6 @@
+module.exports={
+
+inicio:function (Bus, Mejor, durmio){
+	return `Habia una vez una Vaquita que siempre viaja por ${Bus}, ellas se sentia cada vez ${mejor} porque el dia anterior ${durmio} muy bien.`
+}
+}
